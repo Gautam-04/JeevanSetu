4c88f969db4c2472ef02f94411ca089bbc2e5b2e
Gautam-04 <2022.gautam.rai@ves.ac.in>
2025-10-02 15:06:00 +0530

    backend admin setup
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDb from "./config/configDb.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+connectDb();
+
+const PORT = process.env.PORT || 8000;
+
+// import all routers
+import userRouter from './routes/user.routes.js';
+
+app.use('/api/user',userRouter);
+
+
+
+//app commmands
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/',(req,res)=>{
+    res.send('JeevanSetu Backend is running...')
+})
