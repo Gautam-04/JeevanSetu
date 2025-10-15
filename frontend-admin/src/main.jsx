00c5200118ac6aaf19296baecb365792735bdd74
Gautam-04 <2022.gautam.rai@ves.ac.in>
2025-10-15 20:05:20 +0530

    basic statemanagement and frontend setups
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- frontend-admin/src/main.jsx
+++ frontend-admin/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import AdminProvider from './context/AdminContext.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter> 
+    <AdminProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    </AdminProvider>
+  </BrowserRouter>
 )
